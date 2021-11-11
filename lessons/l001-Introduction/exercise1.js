12983ed212ce36876586466737545d257d89e6ed
Sergio Posada Urrea <=>
2021-11-10 21:09:35 -0500

    Add first exercise

---
+++ lessons/l001-Introduction/exercise1.js
@@ -0,0 +1,25 @@
+/*
+
+Ud tiene dos arreglos que guardan la información de la edad de personas y otro que guarda la población de ciudades.
+
+const population = [120000, 5645566,65465465,45646546,64874588,8514465];
+const ages = [12,56,4,12,45,78,63,5,35];
+
+Es decir population[0] indica que en la ciudad X (No importa el nombre de la misma) hay un total de 120.000 habitantes, y la persona
+en la posición 0, o sea, ages[0] tiene una edad de 12 años.
+
+El ejercicio consiste en imprimir las ciudades que tienen una población mayor a 200.000 habitantes y las personas que sean mayores
+de edad.
+
+Temas
+
+Funciones
+Ciclos
+Condicionales
+Declaración de variables
+Scope
+Comparaciones
+
+*/
+const population = [120000, 5645566,65465465,45646546,64874588,8514465];
+const ages = [12,56,4,12,45,78,63,5,35];
