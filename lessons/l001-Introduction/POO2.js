24cb51732102788fc869a98c5fb6aed18fcbe3fd
Sergio Posada Urrea <=>
2021-11-13 15:43:53 -0500

    Add poo2 file

---
+++ lessons/l001-Introduction/POO2.js
@@ -0,0 +1,16 @@
+// class Animal {
+// }
+
+// class Person {
+//   name;
+//   animals; //Array of Animals
+
+//   extremities; //Array de 4 posiciones
+// }
+
+// class Extremity{
+//     person;
+// }
+// class Guard extends Person {}
+// class Child extends Person {}
+
